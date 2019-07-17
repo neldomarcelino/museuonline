a06290eaa1874b365af9e58ae2ccbac6eca07f65
neldomarcelino <guerzeneldo@gmail.com>
2019-07-17 18:10:55 +0200

    pesquisa especie db

--- src/database/configDB.py
+++ src/database/configDB.py
@@ -1,16 +1,16 @@
-config = {
-    'user': 'root',
-    'password': 'digio+*12',
-    'host': '127.0.0.1',
-    'database': 'museuonlinebd',
-    'autocommit': True,
-    'port': 3306
-}
 # config = {
-#     'user': 'b5892407de6f3f',
-#     'password': '1fbd9ab0',
-#     'host': 'us-cdbr-iron-east-02.cleardb.net',
-#     'database': 'heroku_fe2fda8c7cbd379',
+#     'user': 'root',
+#     'password': 'digio+*12',
+#     'host': '127.0.0.1',
+#     'database': 'museuonlinebd',
 #     'autocommit': True,
 #     'port': 3306
 # }
+config = {
+    'user': 'b5892407de6f3f',
+    'password': '1fbd9ab0',
+    'host': 'us-cdbr-iron-east-02.cleardb.net',
+    'database': 'heroku_fe2fda8c7cbd379',
+    'autocommit': True,
+    'port': 3306
+}
